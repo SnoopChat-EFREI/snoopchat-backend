d569cb6f514cf31e48d9e60757dc5f05b5fee947
Victor <victorboyer33@gmail.com>
2021-05-11 18:40:18 +0200

    chat routes

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import users from "./users";
 import geolocalisations from "./geolocalisations";
+import chats from "./chats";
 
 const api = Router();
 
 api.use("/users", users);
 api.use("/geolocalisations", geolocalisations);
+api.use("/chats", chats);
 
 export default api;
