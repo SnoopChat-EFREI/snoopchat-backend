5fc563b9313203072e294104447f7a13dcce3cf2
GUIGUIBOY <guillaume_marcel@hotmail.com>
2021-05-31 17:29:28 +0200

    add messages

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -5,6 +5,7 @@ import users from "./users";
 import friends from "./friends";
 import geolocalisations from "./geolocalisations";
 import chats from "./chats";
+import messages from "./messages"
 
 const api = Router();
 
@@ -12,5 +13,6 @@ api.use("/users", users);
 api.use("/friends", friends);
 api.use("/geolocalisations", geolocalisations);
 api.use("/chats", chats);
+api.use("/messages", messages);
 
 export default api;
